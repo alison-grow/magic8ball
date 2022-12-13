dd3b14afaad7aa04927a7cd35115ab0461a91a35
alison-grow <agrowz98@gmail.com>
2022-12-12 22:03:03 -0800

    added some finishing touches

--- js/main.js
+++ js/main.js
@@ -1,61 +1,17 @@
 // BEGINNING OF TEXT ANIMATION
 
-// var TxtRotate = function(el, toRotate, period) {
-//     this.toRotate = toRotate;
-//     this.el = el;
-//     this.loopNum = 0;
-//     this.period = parseInt(period, 10) || 2000;
-//     this.txt = '';
-//     this.tick();
-//     this.isDeleting = false;
-//   };
-  
-//   TxtRotate.prototype.tick = function() {
-//     var i = this.loopNum % this.toRotate.length;
-//     var fullTxt = this.toRotate[i];
-  
-//     if (this.isDeleting) {
-//       this.txt = fullTxt.substring(0, this.txt.length - 1);
-//     } else {
-//       this.txt = fullTxt.substring(0, this.txt.length + 1);
-//     }
-  
-//     this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
-  
-//     var that = this;
-//     var delta = 300 - Math.random() * 100;
-  
-//     if (this.isDeleting) { delta /= 2; }
-  
-//     if (!this.isDeleting && this.txt === fullTxt) {
-//       delta = this.period;
-//       this.isDeleting = true;
-//     } else if (this.isDeleting && this.txt === '') {
-//       this.isDeleting = false;
-//       this.loopNum++;
-//       delta = 500;
-//     }
-  
-//     setTimeout(function() {
-//       that.tick();
-//     }, delta);
-//   };
-  
-//   window.onload = function() {
-//     var elements = document.getElementsByClassName('txt-rotate');
-//     for (var i=0; i<elements.length; i++) {
-//       var toRotate = elements[i].getAttribute('data-rotate');
-//       var period = elements[i].getAttribute('data-period');
-//       if (toRotate) {
-//         new TxtRotate(elements[i], JSON.parse(toRotate), period);
-//       }
-//     }
 
-//     var css = document.createElement("style");
-//     css.type = "text/css";
-//     css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
-//     document.body.appendChild(css);
-//   };
+var i = 0;
+var txt = 'ask me anything....';
+var speed = 130;
+
+window.onload = function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("askMe").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
 
 // END OF TEXT ANIMATION
 
@@ -79,10 +35,10 @@ const imageArray = [
 	"./img/magic8ball_14.jpg",
 	"./img/magic8ball_15.jpg",
 	"./img/magic8ball_16.jpg",
-  "./img/magic8ball_17.jpg",
-  "./img/magic8ball_18.jpg",
-  "./img/magic8ball_19.jpg",
-  "./img/magic8ball_20.jpg"
+  	"./img/magic8ball_17.jpg",
+  	"./img/magic8ball_18.jpg",
+  	"./img/magic8ball_19.jpg",
+  	"./img/magic8ball_20.jpg"
 ];
 
 const image = document.querySelector("img");
@@ -94,7 +50,7 @@ button.addEventListener("click", () => generateRandomPicture(imageArray));
 function generateRandomPicture(array){
 	let randomNum = Math.floor(Math.random() * array.length); 
 	image.setAttribute("src", array[randomNum]);
-  div.appendChild(image)
+  	div.appendChild(image)
 }
 
 $("#btn").click(function(){
